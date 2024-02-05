c43117409a4caf95a52811814913aea7a151ab90
Christian Duffau-Rasmussen <duffau.christian@gmail.com>
2024-02-05 13:20:08 +0100

    deploy.sh remove 'quick' and add 'deps' and 'push-data' options

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
-QUICK=0
+PUSH_LOCAL_DATA=1
+INSTALL_DEPS=1
 DEPLOY_DIR=/srv/baby-tracker
 
 usage()
 {
-    echo "usage: ./deploy.sh [-q|--quick]"
+    echo "usage: ./deploy.sh [-h|--help] [--deps] [--push-data]"
 }
 
 deploy()
@@ -14,16 +15,20 @@ deploy()
     scp requirements.txt $USER@$REMOTE_HOST:${DEPLOY_DIR}/
     scp main.sh $USER@$REMOTE_HOST:${DEPLOY_DIR}/
     rsync -rav -e ssh  --exclude='__pycache__' ./baby_tracker $USER@$REMOTE_HOST:${DEPLOY_DIR}/
-    scp supervisor.conf $USER@$REMOTE_HOST:/etc/supervisor/conf.d/baby-tracker.conf
     scp create-venv.sh $USER@$REMOTE_HOST:${DEPLOY_DIR}/
-    if [ $QUICK == 0 ]
+
+    if [ $INSTALL_DEPS == 0 ]
     then
         ssh $USER@$REMOTE_HOST "apt-get update && apt-get install python3-venv supervisor sqlite3 -y"
         ssh $USER@$REMOTE_HOST "cd $DEPLOY_DIR && ./create-venv.sh"
-        # Import database from CSV's
+    fi
+
+    if [ $PUSH_LOCAL_DATA == 0 ]
+    then
         rsync -rav -e ssh  --exclude='__pycache__' ./data $USER@$REMOTE_HOST:${DEPLOY_DIR}/
         ssh $USER@$REMOTE_HOST "cd $DEPLOY_DIR && ./data/import.sh"
     fi
+    scp supervisor.conf $USER@$REMOTE_HOST:/etc/supervisor/conf.d/baby-tracker.conf
     ssh $USER@$REMOTE_HOST "supervisorctl reread; supervisorctl update; supervisorctl restart baby_tracker"
 }
 
@@ -31,13 +36,17 @@ deploy()
 
 while [ "$1" != "" ]; do
     case $1 in
-        -q | --quick )
-                                  QUICK=1;;
-        -h | --help )             usage
-                                  exit
-                                  ;;
-        * )                       usage
-                                  exit 1
+        --deps)
+            INSTALL_DEPS=0;; 
+
+        --push-data) 
+            PUSH_LOCAL_DATA=0;;
+
+        -h | --help) 
+            usage;exit 0;;
+
+        *)
+            usage;exit 1;;
     esac
     shift
 done
