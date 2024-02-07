d67fcfb1103af8086f81ab76bae33591fa7cfc71
Christian Duffau-Rasmussen <duffau.christian@gmail.com>
2024-02-07 22:36:34 +0100

    refactor db test use yield fixture for isolation of db state between tests

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -1,7 +1,24 @@
-from datetime import datetime
+import pytest
+from datetime import datetime, timedelta
+
 from baby_tracker import db
 
+
 TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
+@pytest.fixture
+def db_conn():
+    _db_conn = db.init_db(db_file=":memory:")
+    yield _db_conn
+    _db_conn.close()
+
+
+def make_feed_record(
+    from_time = datetime(2021, 5, 18, 6, 38, 30),
+    to_time = datetime(2021, 5, 18, 6, 50, 20)
+):
+    duration = to_time - from_time
+    return (from_time, to_time, duration)
 
 
 def list_tables(conn):
@@ -24,35 +41,32 @@ def test_create_table():
     db.create_table(conn, "CREATE TABLE myothertable ( mycol );")
     tables = list_tables(conn)
     assert tables == ["mytable", "myothertable"]
-    conn.close()
 
 
-def test_init_db():
-    conn = db.init_db(db_file=":memory:")
-    tables = list_tables(conn)
-    assert tables == ["feed", "sleep"]
-    conn.close()
+def test_init_db(db_conn):
+    tables = list_tables(db_conn)
+    assert tables == ["feed", "sleep", "weight", "poop"]
+    db_conn.close()
 
 
-def test_create_feed():
-    conn = db.init_db(db_file=":memory:")
-    from_time = datetime(2021, 5, 18, 6, 38, 30)
-    to_time = datetime(2021, 5, 18, 6, 50, 20)
-    duration = to_time - from_time
-    feed = (from_time, to_time, duration)
-    feed_id = db.create_feed(conn, feed)
-
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM feed;")
+def test_create_feed(db_conn):
+    feed_record = make_feed_record()
+    feed_id = db.create_feed(db_conn, feed_record)
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * FROM feed where id = ?;", (feed_id,))
     rows = cursor.fetchall()
-    print(rows)
-    assert feed_id == 1
     assert len(rows) == 1
-    _id, _from_time, _to_time, _duration, _created_at, _updated_at = rows[0]
-    assert _id == 1
-    assert datetime.strptime(_from_time, TIMESTAMP_FORMAT) == from_time
-    assert datetime.strptime(_to_time, TIMESTAMP_FORMAT) == to_time
-    assert _duration == duration.seconds
-    assert datetime.strptime(_created_at, TIMESTAMP_FORMAT) < datetime.now()
+
+
+def test_get_feed_record(db_conn):
+    from_time, to_time, duration = make_feed_record()
+    feed_id = db.create_feed(db_conn, (from_time, to_time, duration))
+    feed_record = db.get_feed_record_by_id(db_conn, feed_id)
+    _id, _from_time, _to_time, _duration, _created_at, _updated_at = feed_record
+    assert _id == feed_id
+    assert _from_time == from_time
+    assert _to_time == to_time
+    assert _duration == duration
+    assert _created_at < datetime.now()
+    assert _created_at > datetime.now()-timedelta(seconds=1 )
     assert _updated_at is None
-    conn.close()
